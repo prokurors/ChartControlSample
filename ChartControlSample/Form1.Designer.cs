3db398a37204527fa40bb3bf4663d77fc35b7936
Uldis Rāts <uldis.rats@gmail.com>
2024-09-28 18:38:49 +0300

    Add project files.

---
+++ ChartControlSample/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ChartControlSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.ConstantLine constantLine1 = new DevExpress.XtraCharts.ConstantLine();
+            DevExpress.XtraCharts.Strip strip1 = new DevExpress.XtraCharts.Strip();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SeriesPoint seriesPoint1 = new DevExpress.XtraCharts.SeriesPoint(new System.DateTime(2024, 9, 25, 0, 0, 0, 0), new object[] {
+            ((object)(20D)),
+            ((object)(20D)),
+            ((object)(20D)),
+            ((object)(20D)),
+            ((object)(20D)),
+            ((object)(double.NaN))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint2 = new DevExpress.XtraCharts.SeriesPoint(new System.DateTime(2024, 9, 26, 0, 0, 0, 0), new object[] {
+            ((object)(0D)),
+            ((object)(13D)),
+            ((object)(13D)),
+            ((object)(13D)),
+            ((object)(13D)),
+            ((object)(double.NaN))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint3 = new DevExpress.XtraCharts.SeriesPoint(new System.DateTime(2024, 9, 27, 0, 0, 0, 0), new object[] {
+            ((object)(13D)),
+            ((object)(0D)),
+            ((object)(0D)),
+            ((object)(0D)),
+            ((object)(0D)),
+            ((object)(double.NaN))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint4 = new DevExpress.XtraCharts.SeriesPoint(new System.DateTime(2024, 9, 30, 0, 0, 0, 0), new object[] {
+            ((object)(17D)),
+            ((object)(17D)),
+            ((object)(17D)),
+            ((object)(17D)),
+            ((object)(17D)),
+            ((object)(double.NaN))});
+            DevExpress.XtraCharts.StepLineSeriesView stepLineSeriesView1 = new DevExpress.XtraCharts.StepLineSeriesView();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(strip1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(stepLineSeriesView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            constantLine1.AxisValueSerializable = "09/26/2024 00:00:00.000";
+            constantLine1.LegendText = "Specific date (Legend)";
+            constantLine1.Name = "Constant Line 1";
+            constantLine1.ShowBehind = true;
+            constantLine1.Title.Text = "Specific date ";
+            xyDiagram1.AxisX.ConstantLines.AddRange(new DevExpress.XtraCharts.ConstantLine[] {
+            constantLine1});
+            xyDiagram1.AxisX.GridLines.Visible = true;
+            strip1.FillStyle.FillMode = DevExpress.XtraCharts.FillMode.Hatch;
+            strip1.MaxLimit.AxisValueSerializable = "09/28/2024 00:00:00.000";
+            strip1.MinLimit.AxisValueSerializable = "09/27/2024 00:00:00.000";
+            strip1.Name = "Holiday";
+            xyDiagram1.AxisX.Strips.AddRange(new DevExpress.XtraCharts.Strip[] {
+            strip1});
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.MinorCount = 4;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series1.CrosshairEmptyValueLegendText = "Nav dati";
+            series1.DateTimeSummaryOptions.MeasureUnit = DevExpress.XtraCharts.DateTimeMeasureUnit.Hour;
+            series1.DateTimeSummaryOptions.MeasureUnitMultiplier = 1;
+            series1.Name = "Amount";
+            series1.Points.AddRange(new DevExpress.XtraCharts.SeriesPoint[] {
+            seriesPoint1,
+            seriesPoint2,
+            seriesPoint3,
+            seriesPoint4});
+            stepLineSeriesView1.EmptyPointOptions.ProcessPoints = DevExpress.XtraCharts.ProcessEmptyPointsMode.Ignore;
+            series1.View = stepLineSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.Size = new System.Drawing.Size(800, 450);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(strip1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(stepLineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+    }
+}
+
